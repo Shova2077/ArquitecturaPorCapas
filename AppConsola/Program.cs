1112dad8897985d2430111ee0e92c355763b1468
Giovani <borettogiovanni@gmail.com>
2026-04-23 17:40:28 -0300

    Clase Auto

--- AppConsola/Program.cs
+++ AppConsola/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO.Pipes;
+using EjerciciosPOO3;
+
+List<Vehiculo> vehiculos = new List<Vehiculo>()
+{
+    new Auto("Lamborghini", "Gallardo", 2015, 2),
+    new Moto("Susuki", "Hayabusa", 2018, false),
+    new Moto("Susuki", "V-Strom", 2015, true)
+};
+
+foreach (Vehiculo v in vehiculos)
+{
+    Console.WriteLine(v.Describir());
+}
+
+try 
+{
+    Auto autoAntiguo = new Auto("Ford", "T", 1900, 2);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
