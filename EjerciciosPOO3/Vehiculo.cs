ef88edf8077d0b0a26498a3f7d17dd175711266a
Giovani <borettogiovanni@gmail.com>
2026-04-23 16:52:45 -0300

    Clase Vehiculo

---
+++ EjerciciosPOO3/Vehiculo.cs
@@ -0,0 +1,24 @@
+﻿namespace EjerciciosPOO3;
+
+public class Vehiculo
+{
+    public string Marca {get; set;}
+    public string Modelo {get; set;}
+    public int Anio {get; set;}
+
+    public Vehiculo(string marca, string modelo, int anio)
+    {
+        if (anio <= 1900)
+        {
+            throw new ArgumentExcpetion("El vehiculo es muy antiguo")
+        }
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+    }
+    
+    virtual string Describir()
+    {
+        return $"{Marca} {Modelo} {(Anio)}";
+    }
+}
