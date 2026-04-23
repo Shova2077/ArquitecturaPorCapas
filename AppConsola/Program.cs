9e1bb1c3fad2c4065a62974c8422ebcf515ebb07
Giovani <borettogiovanni@gmail.com>
2026-04-23 17:50:59 -0300

    Programa final

---
+++ AppConsola/Program.cs
@@ -0,0 +1,23 @@
+using System.IO.Pipes;
+using EjerciciosPOO3;
+
+List<Vehiculo> vehiculos = new List<Vehiculo>()
+{
+    new Auto("Lamborghini", "Gallardo", 2015, 2),
+    new Moto("Susuki", "Hayabusa", 2018, false),
+    new Moto("Susuki", "V-Strom", 2015, true)
+};
+
+foreach (Vehiculo v in vehiculos)
+{
+    Console.WriteLine(v.Describir());
+}
+
+try 
+{
+    Auto autoAntiguo = new Auto("Ford", "T", 1900, 2);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
