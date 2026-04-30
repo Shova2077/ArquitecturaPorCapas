36720a47ec38c473bc306f40133106ad3b66ab65
Giovani Boretto <borettogiovanni@gmail.com>
2026-04-30 19:18:13 -0300

    Se edito la clase BibliotecaGestor

--- EjerciciosPOO3/Biblioteca/BibliotecaGestor.cs
+++ EjerciciosPOO3/Biblioteca/BibliotecaGestor.cs
@@ -2,4 +2,13 @@ namespace EjerciciosPOO3;
 
 public class BibliotecaGestor
 {
+    public void ListarPublicaciones(List<Libro> libros, List<Revista> revistas)
+    {
+        foreach (Libro l in libros)
+        {
+            List<Libro> librosordenados = libros.OrderByDescending(l => l.Anio).ToList();
+            Console.WriteLine("Ordenados por precio");
+            Console.WriteLine(l.Describir());
+        }
+    }
 }
